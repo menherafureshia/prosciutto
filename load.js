013486f47922fe3563065d1f43487c9e328f92d0
「ひとり」深見 <113910432+furukawahitori@users.noreply.github.com>
2026-01-31 14:23:41 +0800

    Update load.js

--- load.js
+++ load.js
@@ -1,5 +1,5 @@
 //a modification for Koukou Seikatsu
-$gameMessage.add("init successfully loaded from\nhttps://raw.githubusercontent.com/furukawahitori/prosciutto/refs\n/heads/main/load.js");
+$gameMessage.add("init successfully loaded from\nMenherafureshia's Prosciutto Modification Load.");
 $gameParty.addActor(2);
 $gameParty.addActor(3);
 $gameParty.addActor(4);
